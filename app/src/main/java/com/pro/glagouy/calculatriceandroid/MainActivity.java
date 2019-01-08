533fddab0ae59a8ea2e994da1729b7f11319803a
Sebastien Hernoux <seb_her@hotmail.fr>
2019-01-08 12:03:04 +0100

    add buttons click listeners

--- app/src/main/java/com/pro/glagouy/calculatriceandroid/MainActivity.java
+++ app/src/main/java/com/pro/glagouy/calculatriceandroid/MainActivity.java
@@ -2,12 +2,42 @@ package com.pro.glagouy.calculatriceandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private String viewValue;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.textView = findViewById(R.id.tv_result);
+    }
+
+    private void updateView() {
+        this.textView.setText(this.viewValue);
+    }
+
+    /**
+     *
+     * @param view
+     */
+    public void numberClick(View view) {
+        int valuePress = Integer.parseInt(view.getTag().toString());
+        this.viewValue += valuePress;
+        System.out.println(valuePress);
+        updateView();
+    }
+
+    /**
+     *
+     * @param view
+     */
+    public void operationClick(View view) {
+        String valuePress = (String) view.getTag();
+        this.viewValue += valuePress;
+        updateView();
     }
 }
