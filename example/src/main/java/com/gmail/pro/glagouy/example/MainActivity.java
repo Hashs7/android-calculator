6923911a1f43063e31cf687cc9350faa7f84482d
GuillaumeLagouy <glagouy.pro@gmail.com>
2019-01-09 09:28:50 +0100

    Layout line item

--- example/src/main/java/com/gmail/pro/glagouy/example/MainActivity.java
+++ example/src/main/java/com/gmail/pro/glagouy/example/MainActivity.java
@@ -15,20 +15,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        List<String> planetes = new ArrayList<>();
-        planetes.add("MERCURE");
-        planetes.add("VENUS");
-        planetes.add("TERRE");
-        planetes.add("MARS");
-        planetes.add("JUPITER");
-        planetes.add("SATURNE");
-        planetes.add("URANUS");
-        planetes.add("NEPTUNE");
-
         Spinner sp = (Spinner) findViewById(R.id.spinner1);
+        String[] planetes = getResources().getStringArray(R.array.planetesArray);
+
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, planetes);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         sp.setAdapter(dataAdapter);
+
+
 
     }
 }
