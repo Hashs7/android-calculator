0db9b21a39d14a5c2da8a79eb9f9f1a887cfdd15
Sebastien Hernoux <seb_her@hotmail.fr>
2019-01-09 09:02:23 +0100

    crap

--- example/src/main/java/com/gmail/pro/glagouy/example/MainActivity.java
+++ example/src/main/java/com/gmail/pro/glagouy/example/MainActivity.java
@@ -2,6 +2,8 @@ package com.gmail.pro.glagouy.example;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Spinner sp = (Spinner) findViewById(R.id.spinner1);
     }
+
 }
