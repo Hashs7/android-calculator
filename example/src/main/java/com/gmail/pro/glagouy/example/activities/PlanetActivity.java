1854fb5e20783198f6376f92469cc2f69d73411c
GuillaumeLagouy <glagouy.pro@gmail.com>
2019-01-09 14:21:09 +0100

    Ajout d'une vue détaillée

--- example/src/main/java/com/gmail/pro/glagouy/example/activities/PlanetActivity.java
+++ example/src/main/java/com/gmail/pro/glagouy/example/activities/PlanetActivity.java
@@ -1,9 +1,14 @@
 package com.gmail.pro.glagouy.example.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.MenuView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.gmail.pro.glagouy.example.R;
+import com.squareup.picasso.Picasso;
 
 public class PlanetActivity extends AppCompatActivity {
 
@@ -11,5 +16,18 @@ public class PlanetActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_planet);
+
+        Intent intent = getIntent();
+        String title = intent.getStringExtra("title");
+        String desc = intent.getStringExtra("desc");
+        String img = intent.getStringExtra("img");
+
+        TextView textViewTitle = findViewById(R.id.tv_item_planetTitle);
+        TextView textViewDesc = findViewById(R.id.tv_item_planetDesc);
+        ImageView imageView = findViewById(R.id.iv_item_planetImg);
+
+        textViewTitle.setText(title);
+        textViewDesc.setText(desc);
+        Picasso.get().load(img).into(imageView);
     }
 }
